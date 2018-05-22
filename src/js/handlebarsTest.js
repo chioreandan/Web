8fb105a164a50cd346f0aef1f8990590a32c4c24
dan <chiorean.dan12@gmail.com>
2018-05-22 21:55:33 +0300

    Product filtering is now working properly

--- src/js/handlebarsTest.js
+++ src/js/handlebarsTest.js
@@ -1,38 +1,38 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-  let leftPrice=document.querySelector('#left-price');
-  let rightPrice=document.querySelector('#right-price')
+  let leftPrice = document.querySelector('#left-price');
+  let rightPrice = document.querySelector('#right-price')
   let dropwdown_item = document.querySelector(".category-dropdown");
-  let delFiters=document.querySelector("#del-filters");
+  let delFiters = document.querySelector("#del-filters");
   $(window).scroll(function () {
     $('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
-});
+  });
   menu = JSON.parse(localStorage.getItem("savedMenu"));
-
   let slider2 = document.querySelector("#range2");
 
-
-
-
-
-  function filterByCategory(menu, category) {
+  function filterByPrice(menu, price) {
+    let res = price.split(",");
+    maxPrice = parseInt(res[1]);
+    minPrice = parseInt(res[0]);
+    leftPrice.innerHTML = "&#36;" + minPrice;
+    rightPrice.innerHTML = "&#36;" + maxPrice;
     let filtredMenu = [];
     for (let i = 0; i < menu.length; i++) {
-      if (menu[i].category === category) {
+      if (menu[i].price >= minPrice && menu[i].price <= maxPrice) {
         filtredMenu.push(menu[i]);
       }
     }
     return filtredMenu;
   }
 
-  function filterByPrice(menu, price) {
-    let res=price.split(",");
-    maxPrice=parseInt(res[1]);
-    minPrice=parseInt(res[0]);
-    leftPrice.innerHTML="&#36;"+minPrice;
-    rightPrice.innerHTML="&#36;"+maxPrice;
+  function filter(menu, price, category) {
+    let res = price.split(",");
+    maxPrice = parseInt(res[1]);
+    minPrice = parseInt(res[0]);
+    leftPrice.innerHTML = "&#36;" + minPrice;
+    rightPrice.innerHTML = "&#36;" + maxPrice;
     let filtredMenu = [];
     for (let i = 0; i < menu.length; i++) {
-      if (menu[i].price >= minPrice && menu[i].price <= maxPrice) {
+      if (menu[i].price >= minPrice && menu[i].price <= maxPrice && menu[i].category === category) {
         filtredMenu.push(menu[i]);
       }
     }
@@ -43,38 +43,37 @@ document.addEventListener("DOMContentLoaded", function (event) {
     var template = Handlebars.compile(data);
     var test = document.querySelector('#handle-test');
     $('index.html').html();
-    let k=menu;
+    let k = menu;
+    let currentCategory = '';
     renderMenu(k);
-    dropwdown_item.addEventListener("click",function(e){
-      e.preventDefault();
-      console.log(e.target.innerHTML);
-      let p = filterByCategory(k, e.target.innerHTML);
-      k=p;
-      renderMenu(p);
-    })
-    slider2.addEventListener("click",function(e){
-      slider2.ghost;
-      k=filterByPrice(menu,e.target.value);
-      renderMenu(k);
 
-    })
-    delFiters.addEventListener("click",function(e){
-      e.preventDefault();
-      renderMenu(menu);
-    })
-
-
-    function renderMenu(k){
-      test.innerHTML='';
+    function renderMenu(k) {
+      test.innerHTML = '';
       for (let i = 0; i < k.length; i++) {
         var html = template(k[i]);
         test.innerHTML += html;
-
       }
     }
+    dropwdown_item.addEventListener("click", function (e) {
+      e.preventDefault();
+      currentCategory = e.target.innerHTML;
+      k = filter(menu, slider2.value, e.target.innerHTML);
+      console.log(k);
+      renderMenu(k);
+    })
+    slider2.addEventListener("click", function (e) {
+      console.log(e.target.value);
+      if (currentCategory != null) {
+        k = filter(menu, slider2.value, currentCategory);
+      } else {
+        k = filterByPrice(menu, currentCategory);
+      }
 
-
+      renderMenu(k);
+    })
+    delFiters.addEventListener("click", function (e) {
+      e.preventDefault();
+      renderMenu(menu);
+    })
   })
-
-
 }, 'html')
